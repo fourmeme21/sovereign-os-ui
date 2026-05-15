cc58c478b0c425a7ab110eeb990decf51bf96b03
Lionel <126454243+fourmeme21@users.noreply.github.com>
2026-05-15 04:15:51 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import AppRouter from "./AppRouter";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <AppRouter />
+    </BrowserRouter>
+  </React.StrictMode>
 );
