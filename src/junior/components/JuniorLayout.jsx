80a95c39a694d2ead0e859178905419a27fcbb64
Lionel <126454243+fourmeme21@users.noreply.github.com>
2026-05-16 03:19:33 +0100

    Integrate engine status check in JuniorLayout
    Add engine status check and offline bar to layout

--- src/junior/components/JuniorLayout.jsx
+++ src/junior/components/JuniorLayout.jsx
@@ -1,11 +1,16 @@
 import { Outlet } from "react-router-dom";
 import JuniorNav from "./JuniorNav";
 import JuniorStatusBar from "./JuniorStatusBar";
+import { EngineOfflineBar } from "./ErrorStates";
+import { useEngineStatus } from "../hooks/useEngineStatus";
 import "./ekranlar.css";
 
 export default function JuniorLayout() {
+  const engineOnline = useEngineStatus();
+
   return (
     <div className="junior-layout">
+      {!engineOnline && <EngineOfflineBar />}
       <JuniorNav />
       <div className="junior-content">
         <Outlet />
