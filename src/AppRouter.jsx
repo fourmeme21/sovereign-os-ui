9f1a8d79b5161c7c482acb2b683555477416d95c
Lionel <126454243+fourmeme21@users.noreply.github.com>
2026-05-16 14:57:42 +0100

    Update AppRouter.jsx

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import App from "./App";
+import LandingPage from "./screens/LandingPage";
 import JuniorLayout from "./junior/components/JuniorLayout";
 import KararGecmisi from "./junior/screens/KararGecmisi";
 import ProjHafizasi from "./junior/screens/ProjHafizasi";
@@ -8,9 +8,9 @@ import Baglan from "./junior/screens/Baglan";
 export default function AppRouter() {
   return (
     <Routes>
-      <Route path="/" element={<App />} />
+      <Route path="/" element={<LandingPage />} />
       <Route path="/junior" element={<JuniorLayout />}>
-        <Route index       element={<KararGecmisi />} />
+        <Route index         element={<KararGecmisi />} />
         <Route path="gecmis" element={<KararGecmisi />} />
         <Route path="hafiza" element={<ProjHafizasi />} />
         <Route path="baglan" element={<Baglan />} />
