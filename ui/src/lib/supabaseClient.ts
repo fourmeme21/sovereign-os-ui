e6fcf028d1f03461990485520c0936c1dd37b349
Lionel <126454243+fourmeme21@users.noreply.github.com>
2026-05-18 07:16:22 +0100

    Update supabaseClient.ts

--- ui/src/lib/supabaseClient.ts
+++ ui/src/lib/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL  = "https://gicmzsybkrzxeavdjkxc.supabase.co";
-const SUPABASE_ANON = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdpY216c3lia3J6eGVhdmRqa3hjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg2NDI1MjksImV4cCI6MjA5NDIxODUyOX0.xu-CRxEh1DGJCJfte4zaHhjE71bpo5XZUCqhfHKupV0";
+const SUPABASE_URL  = import.meta.env.VITE_SUPABASE_URL;
+const SUPABASE_ANON = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON);
