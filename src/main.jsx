ee7cb439c8e327443b6c12b6ea2ac8a8e15847b5
Lionel <126454243+fourmeme21@users.noreply.github.com>
2026-06-06 03:59:30 +0000

    fix: initAuthListener registerSession injection (#15)

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,11 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import AppRouter from "./AppRouter";
 import { initAuthListener } from "./stores/authStore";
+import { registerSession }  from "./junior/hooks/useAuth";
 import { checkForUpdates } from "./lib/updater";
 import "./i18n";  // ← EKLE
 
-initAuthListener();
+initAuthListener(registerSession);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
